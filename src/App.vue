616a8f6f561a9eef73949aea78ab851887195788
andrew-wilkes <github@urgentclick.com>
2023-02-21 11:48:13 +0900

    Initial setup

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Grid from './components/Grid.vue'
+import UI from './components/UI.vue'
+</script>
+
+<template>
+  <div class="frame">
+    <Grid />
+    <UI />
+  </div>
+</template>
+
+<style scoped>
+  .frame {
+    display: flex;
+  }
+</style>
---
+++ src/components/UI.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="ui">
+    This is the UI.
+  </div>
+</template>
