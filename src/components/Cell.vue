75df6a60e7158148cb7f38a26f245f456106206e
andrew-wilkes <github@urgentclick.com>
2023-02-22 16:02:15 +0900

    Add to cell component

--- src/components/Cell.vue
+++ src/components/Cell.vue
@@ -1,5 +1,37 @@
 <template>
     <div class="cell">
-        Cell
+        <button v-if="candidates.length < 2" type="button" class="cell cell-top" :class="{ given: isGiven }">{{ topValue }}</button>
+        <button v-else-if="candidates.length < 3" type="button" class="cell cell-str">
+            <span v-for="n in candidates" :class="'n' + n">{{ n }}</span>
+        </button>
+        <button v-else type="button" class="cell cell-grid">
+            <div v-for="n in 9" class="num" :class="'n' + n">{{ candidate(n) }}</div>
+        </button>
     </div>
-  </template>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+        number: 8,
+        candidates: "1289"
+    }
+  },
+  computed: {
+    topValue() {
+        if (this.number == 0) return "";
+        if (this.number > 10) return this.number - 10;
+        return this.number;
+    },
+    isGiven() {
+        return this.number > 10;
+    }
+  },
+  methods: {
+    candidate(n) {
+        return this.candidates.indexOf('' + n) > -1 ? n : '';
+    }
+  }
+}
+</script>
